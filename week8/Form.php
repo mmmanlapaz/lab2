924b6af78269f863879dcd9c0ad0df4c412d7d6d
mmmanlapaz <mmmanlapaz@student.apc.edu.ph>
2024-02-12 21:47:51 +0800

    updated php

--- week8/Form.php
+++ week8/Form.php
@@ -100,5 +100,31 @@ echo "<br>";
 echo $gender;
 ?>
 
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "myDB";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "INSERT INTO MyGuests (name,email,website,comment,gender)
+VALUES ('$name', '$email', '$website', '$comment', '$gender')";
+
+
+if (mysqli_query($conn, $sql)) {
+  echo "Record updated successfully";
+} else {
+  echo "Error updating record: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
+
 </body>
 </html>
